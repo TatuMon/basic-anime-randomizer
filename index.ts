0e8856522fafd09c127cfe47c5ac29fee8008f8f
Juan Cruz Montiel <juan.montiel@paians.com>
2023-09-11 11:42:20 -0300

    Added home route

--- index.ts
+++ index.ts
@@ -2,14 +2,18 @@ import { env } from "process";
 import router, { handleRequest } from "./src/lib/router";
 import randomize from "./src/randomize";
 
+router.get("/", () => {
+    return new Response("Welcome! Go to /randomize to get a random anime");
+});
+
 router.get("/sayHi", () => {
     return new Response("Hii");
-})
+});
 
 router.get("/randomize", (request) => {
     //TODO
     return randomize(request);
-})
+});
 
 Bun.serve({
     port: env.PORT,
